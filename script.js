3c3867e8426e1055a15cdad05650a4836c5ce4fe
DAVID_THE_POOP <93525069+DAVIDTHEPOOP@users.noreply.github.com>
2024-01-24 19:16:57 +0100

    Update script.js

--- script.js
+++ script.js
@@ -11,18 +11,6 @@ image.addEventListener("click", function() {
 
 var anchors = document.querySelectorAll('a[data-href]');
 
-/*
-for (var i=0; i<anchors.length; ++i) {
-  var anchor = anchors[i];
-  var href = anchor.getAttribute('data-href');
-  anchor.addEventListener('click', function() {
-    window.location = href;
-  });
-}
-*/
-
-var anchors = document.querySelectorAll('a[data-href]');
-
 for (var i=0; i<anchors.length; ++i) {
   var anchor = anchors[i];
   var href = anchor.getAttribute('data-href');
@@ -32,33 +20,27 @@ for (var i=0; i<anchors.length; ++i) {
   });
 }
 
-(function emojiCursor() {
-  
+function MatrixCursor() {
   var possibleEmoji = ["1", "0"]
   var width = window.innerWidth;
   var height = window.innerHeight;
   var cursor = {x: width/2, y: width/2};
   var particles = [];
-  
   function init() {
     bindEvents();
     loop();
   }
-  
-  // Bind events that are needed
+
   function bindEvents() {
     document.addEventListener('mousemove', onMouseMove);
     document.addEventListener('touchmove', onTouchMove);
     document.addEventListener('touchstart', onTouchMove);
-    
     window.addEventListener('resize', onWindowResize);
   }
-  
   function onWindowResize(e) {
     width = window.innerWidth;
     height = window.innerHeight;
   }
-  
   function onTouchMove(e) {
     if( e.touches.length > 0 ) {
       for( var i = 0; i < e.touches.length; i++ ) {
@@ -66,49 +48,35 @@ for (var i=0; i<anchors.length; ++i) {
       }
     }
   }
-  
   function onMouseMove(e) {    
     cursor.x = e.clientX;
     cursor.y = e.clientY;
-    
     addParticle( cursor.x, cursor.y, possibleEmoji[Math.floor(Math.random()*possibleEmoji.length)]);
   }
-  
   function addParticle(x, y, character) {
     var particle = new Particle();
     particle.init(x, y, character);
     particles.push(particle);
   }
-  
   function updateParticles() {
-    
-    // Updated
     for( var i = 0; i < particles.length; i++ ) {
       particles[i].update();
     }
-    
-    // Remove dead particles
     for( var i = particles.length -1; i >= 0; i-- ) {
       if( particles[i].lifeSpan < 0 ) {
         particles[i].die();
         particles.splice(i, 1);
       }
     }
-    
   }
-  
   function loop() {
     requestAnimationFrame(loop);
     updateParticles();
   }
-  
-  /**
-   * Particles
-   */
-  
-  function Particle() {
 
-    this.lifeSpan = 30; //ms
+  function Particle() {
+  
+    this.lifeSpan = 30;
     this.initialStyles ={
       "position": "absolute",
       "display": "block",
@@ -117,52 +85,38 @@ for (var i=0; i<anchors.length; ++i) {
       "fontSize": "64px",
       "will-change": "transform",
       "color": "#0d0",
-      "margin-left": "-15px",
-      "margin-top": "-35px"
+      "font-family": "mnospace"
     };
 
-    // Init, and set properties
     this.init = function(x, y, character) {
-
+    
       this.velocity = {
         x:  (Math.random() < 0.5 ? -1 : 1) * (Math.random() / 2),
-        y: 1
+        y: 2
       };
-      
-      this.position = {x: x - 10, y: y - 20};
-
+      this.position = {x: x - 26, y: y - 45};
+    
       this.element = document.createElement('span');
       this.element.innerHTML = character;
       applyProperties(this.element, this.initialStyles);
       this.update();
-      
       document.body.appendChild(this.element);
     };
-    
     this.update = function() {
       this.position.x += this.velocity.x;
       this.position.y += this.velocity.y;
       this.lifeSpan--;
-      
       this.element.style.transform = "translate3d(" + this.position.x + "px," + this.position.y + "px,0) scale(" + (this.lifeSpan / 120) + ")";
     }
-    
     this.die = function() {
       this.element.parentNode.removeChild(this.element);
     }
-    
   }
-  
-  /**
-   * Utils
-   */
-  
-  // Applies css `properties` to an element.
+
   function applyProperties( target, properties ) {
     for( var key in properties ) {
       target.style[ key ] = properties[ key ];
     }
   }
-  
   init();
-})();
+}
